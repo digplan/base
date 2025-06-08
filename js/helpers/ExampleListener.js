2d959745106c35beb3541c0bf9ee5746aae406ce
Chris Borkert <digplan@outlook.com>
2025-06-08 08:50:20 -0500

    fix

--- js/helpers/ExampleListener.js
+++
@@ -1,7 +0,0 @@
-Base.ExampleListener ??= class ExampleListener {
-    static onMouseEnter() {
-       $('status').innerHTML += 'mouse entered (listened from non-element)';
-    }
-}
-
-export default Base.ExampleListener;
