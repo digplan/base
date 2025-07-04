337e1560b2e08fc54dd4173420561d74944e27bc
Chris Borkert <digplan@outlook.com>
2025-07-03 23:49:21 -0500

    storage by user name

--- base-classes/Storage.js
+++ base-classes/Storage.js
@@ -2,6 +2,7 @@ const storage = (() => {
   const obj = Object.create(null);
 
   obj.get = (type, key) => {
+    type = `${user?.shortId}:${type}`;
     if (type == '' || key == '') throw new Error('Invalid type or key');
     if (key === '*') {
       const arr = [];
@@ -19,28 +20,32 @@ const storage = (() => {
   };
 
   obj.set = (type, key, value) => {
+    type = `${user?.shortId}:${type}`;
     if (typeof value !== 'object') throw new Error('Value must be an object');
     localStorage.setItem(`${type}:${key}`, JSON.stringify(value));
     return value;
   };
 
   obj.delete = (type, key) => {
+    type = `${user?.shortId}:${type}`;
     localStorage.removeItem(`${type}:${key}`);
     return localStorage.getItem(`${type}:${key}`) == null;
   };
 
   obj.saveSetting = (key, value) => {
-    const clientSettings = obj.get("Settings", "Client") || {};
+    const type = `${user?.shortId}:Settings`;
+    const clientSettings = obj.get(type, "Client") || {};
     clientSettings[key] = value;
-    obj.set("Settings", "Client", clientSettings);
+    obj.set(type, "Client", clientSettings);
     return value;
   };
 
   obj.getSetting = (key) => {
+    const type = `${user?.shortId}:Settings`;
     if (key === '*') {
-      return obj.get("Settings", "Client");
+      return obj.get(type, "Client");
     }
-    return obj.get("Settings", "Client")?.[key];
+    return obj.get(type, "Client")?.[key];
   };
 
   return obj;
